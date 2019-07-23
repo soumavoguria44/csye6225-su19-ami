0b67d04bdbe3ee53e6fae7e11a0215be26f2644f
avitiwari008 <tiwari.av@husky.neu.edu>
2019-07-22 21:36:33 -0400

    making req changes

--- requirements.sh
+++ requirements.sh
@@ -172,14 +172,16 @@ EOF
 
     sudo wget https://s3.amazonaws.com/amazoncloudwatch-agent/centos/amd64/latest/amazon-cloudwatch-agent.rpm
     sudo rpm -U ./amazon-cloudwatch-agent.rpm
-    
+    sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/cloudwatch-config.json -s
+                          
 
     cd ~
 
     sudo wget https://s3.amazonaws.com/configfileforcloudwatch/amazon-cloudwatch-agent.service
-    sudo cp amazon-cloudwatch-agent.service /usr/lib/systemd/system/
+    sudo cp amazon-cloudwatch-agent.service /etc/systemd/system/
     sudo systemctl enable amazon-cloudwatch-agent.service
-    
+    sudo systemctl start amazon-cloudwatch-agent.service
+    sudo systemctl status amazon-cloudwatch-agent.service
     sudo echo 'SUCCESS!!!!!!!'
 
     exit 0
